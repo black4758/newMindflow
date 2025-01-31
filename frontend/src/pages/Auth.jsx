2d8a4bec74c850d3eda06095559379ac8aea6ae8
이강민 <2mini0329@naver.com>
2025-01-31 17:48:53 +0900

    fix: 로그인 메소드 수정

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function Login() {
-	const [username, setUsername] = useState("");
+	const [accountId, setAccountId] = useState("");
 	const [password, setPassword] = useState("");
 	// const [token, setToken] = useState(null);
 
 	const handleLogin = async () => {
 		try {
-			const response = await axios.post("http://localhost:5173", {
-				username,
+			const response = await axios.post("http://localhost:8453/api/auth/login", {
+				accountId,
 				password
 			});
 			// setToken(response.data.token);
 			// localStorage.setItem("token", response.data.token);
-			console.log(username, password)
+			console.log(accountId, password)
 			console.log("전송 성공")
 		} catch(error) {
 			console.error("로그인 실패");
@@ -23,7 +23,7 @@ function Login() {
 
 	return (
 		<div>
-			<input type="text" placeholder="아이디" onChange={(e) => setUsername(e.target.value)} />
+			<input type="text" placeholder="아이디" onChange={(e) => setAccountId(e.target.value)} />
 			<input type="password" placeholder="비밀번호" onChange={(e) => setPassword(e.target.value)} />
 			<button onClick={handleLogin}>로그인</button>
 		</div>
