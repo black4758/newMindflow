39ff23ac288f6dd96aa7b65d88e9c00623000401
김세현 <kimsa1335@naver.com>
2025-02-03 16:12:33 +0900

    feat: 회원 가입 페이지 생성 & 사이드바 접기

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,33 +1,41 @@
-import MainPage from "../pages/MainPage.jsx";
-import Login from "../pages/Login.jsx";
-import MindmapPage from "../pages/MindmapPage.jsx";
-import OnedataPage from "../pages/OnedataPage.jsx";
+import MainPage from "../pages/MainPage.jsx"
+import Login from "../pages/Login.jsx"
+import MindmapPage from "../pages/MindmapPage.jsx"
+import OnedataPage from "../pages/OnedataPage.jsx"
+import Signup from "../pages/Signup.jsx"
 
 const routes = [
-    {
-        path: '/',
-        element: <MainPage />,
-        title: '메인페이지',
-        requireAuth: false
-    },
-    {
-        path: '/login/',
-        element: <Login />,
-        title: '로그인페이지',
-        requireAuth: false
-    },
-    {
-        path: '/mindmap/',
-        element: <MindmapPage />,
-        title: '마인드맵페이지',
-        requireAuth: false
-    },
-    {
-        path: '/onedata/',
-        element: <OnedataPage />,
-        title: '강조페이지',
-        requireAuth: false
-    },
-];
+  {
+    path: "/",
+    element: <MainPage />,
+    title: "메인페이지",
+    requireAuth: false,
+  },
+  {
+    path: "/login/",
+    element: <Login />,
+    title: "로그인페이지",
+    requireAuth: false,
+  },
+  {
+    path: "/signup/",
+    element: <Signup />,
+    title: "회원가입페이지",
+    requireAuth: false,
+  },
+  {
+    path: "/mindmap/",
+    element: <MindmapPage />,
+    title: "마인드맵페이지",
+    requireAuth: false,
+  },
+
+  {
+    path: "/onedata/",
+    element: <OnedataPage />,
+    title: "강조페이지",
+    requireAuth: false,
+  },
+]
 
 export default routes
