fdb323309acb124aaf6644700923d8a6f5c887e9
HuiSeopKwak <kh06033@naver.com>
2025-02-06 02:34:54 +0900

    feat: NGINX 연결 확인 및 오류 수정중

--- backend/src/main/java/com/swissclassic/mindflow_server/config/CorsConfig.java
+++ backend/src/main/java/com/swissclassic/mindflow_server/config/CorsConfig.java
@@ -32,7 +32,7 @@ public class CorsConfig {
         config.addAllowedMethod("OPTIONS");
 
         source.registerCorsConfiguration("/api/**", config);
-        source.registerCorsConfiguration("/v3/api-docs/**", config);  // 추가
+        source.registerCorsConfiguration("/v3/api-docs", config);  // 추가
         source.registerCorsConfiguration("/swagger-ui/**", config);   // 추가
 
         return new CorsFilter(source);
