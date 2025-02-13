66fcddd502d4f7d1096e17d1595ee1715e66203e
이강민 <2mini0329@naver.com>
2025-02-13 17:26:32 +0900

    fix: cors 이슈가 터지던 현상 수정

--- backend/src/main/java/com/swissclassic/mindflow_server/config/CorsConfig.java
+++ backend/src/main/java/com/swissclassic/mindflow_server/config/CorsConfig.java
@@ -1,25 +1,29 @@
 package com.swissclassic.mindflow_server.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
+@Slf4j
 public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-
+        List<String> allowedOrigins = List.of("http://localhost:5173", "http://localhost:5174", "http://localhost",
+                                              "http://i12d203.p.REDACTED.io", "https://mindflow.ddns.net",
+                                              "http://localhost:8453"
+        );
+        log.error("CorsFilter" + allowedOrigins.toString());
+        config.setAllowedOrigins(allowedOrigins);
         // 허용할 origin 설정 (React 앱 주소)
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("http://localhost:5174");
-        config.addAllowedOrigin("http://i12d203.p.REDACTED.io");
-        config.addAllowedOrigin("https://mindflow.ddns.net");
-
         // 자격증명 허용
         config.setAllowCredentials(true);
 
