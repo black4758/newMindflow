07e4bf5ebe1b91bc7b4a16452ed989d18d0a2cac
woo seong yun <arblack475867@gmail.com>
2026-01-09 16:33:36 +0900

    chore: restore proper environment variable configurations after history redaction

--- init-mongo.js
+++ init-mongo.js
@@ -1,16 +1,16 @@
-// root 계정이 있는지 확인 후 생성 (이미 존재하면 오류 방지)
+// root 계정이 있는지 확인 후 생성
 db.createUser({
   user: "root",
-  pwd: "REDACTED",
+  pwd: process.env.MONGO_ROOT_PASSWORD || "1234",
   roles: [{ role: "root", db: "admin" }]
 });
 
 db = db.getSiblingDB("mindflow_db");
 
-// mindflow_db에 "REDACTED" 계정 추가
+// mindflow_db에 계정 추가
 db.createUser({
-  user: "REDACTED",
-  pwd: "REDACTED",
+  user: process.env.MONGO_USERNAME || "ssafy",
+  pwd: process.env.MONGO_PASSWORD || "ssafy",
   roles: [{ role: "readWrite", db: "mindflow_db" }]
 });
 
